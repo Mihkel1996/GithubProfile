0fb3426124853b00d935da0230b924c06f0b37c1
Mihkel1996 <mihkeljohannes.paimla@gmail.com>
2020-01-14 11:54:08 +0200

    Add files via upload

---
+++ github.js
@@ -0,0 +1,18 @@
+class GitHub {
+
+    constructor(){
+        this.client_ID = "de0b084f60a20c35fc4d";
+        this.client_Secret = "7e11770338448aba7a9e587372e717e2cd9f5abf";
+        this.user = 'Mihkel1996';
+        this.apiUrl = "https://api.github.com/users/"
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+    }   
+
+    async getUserData(){
+        let urlProfile = `${this.apiUrl}${this.user}?client_id=${this.client_ID}&client_secret=${this.client_secret}`;
+        let urlRepos = `${this.apiUrl}${this.user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_ID}&client_secret=${this.client_Secret}`;
+    }
+
+
+}
